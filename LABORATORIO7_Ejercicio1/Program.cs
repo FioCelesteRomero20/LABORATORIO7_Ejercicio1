2efe9d6f7467ffc73fd4836de7669697fbfc4f21
PC <PC@DESKTOP-NOE8GP1>
2023-10-04 23:27:11 -0500

    Agregar archivos de proyecto.

---
+++ LABORATORIO7_Ejercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO7_Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.Write("Ingrese el valor de n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                double suma = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    double termino = (16 - i) / (3.0 * i);
+                    suma += termino;
+                    Console.WriteLine($"resultado: {suma} para i={i}");
+                }
+
+                Console.WriteLine($"La suma de la secuencia para n={n} es: {suma}");
+            
+        }
+    }
+}
